943df38d14ff7318bb3b1a487a14fff078de6f58
arisukarno25 <arisukarno.25@gmail.com>
2022-08-13 15:47:12 +0700

    update tf version

--- 3 tier - VPC/version.tf
+++ 3 tier - VPC/version.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 1.27"
+  required_version = "~> 1.2.1" #~> symbol allows the patch version to be greater than 1 but requires the major and minor versions (1.1)
   required_providers {
     aws = {
       source  = "hasichorp/aws"
-      version = "~> 3.14.2"
+      version = "~> 3.14.0"
     }
   }
 }
